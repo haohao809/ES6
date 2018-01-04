15cb99698c921660905cc8779f0a1eeea9e40270
haohao809 <haohao809@126.com>
2018-01-04 16:43:32 +0800

    base.js

--- app/js/lottery/base.js
+++ app/js/lottery/base.js
@@ -125,7 +125,94 @@ import $ from 'jquery'
 		}
 		//添加号码
 		addCode(){
+			let self = this;
+			let $active = $('.btn-boll-active').text().match(/\d{2}/);
+			let actice = $active?$active.length:0;
+			let count = self.computeCount(active,self.cur_play);
+			if(count){
+				self.addCodeItem($active.join(' '),self.cur_play,self.play_list.get(self.cur_play).name,count);
+			}
 			
 		}
-		
+		//添加单次号码
+		addCodeItem(code,type,typeName,count){
+			let self=this;
+			const tpl =`
+				<li codes="${type}|${code}" bonus="${count*2}" count="${count}">
+					 <div class="code">
+						 <b>${typeName}${count>1? '复式':'单式'}</b>
+						 <b class="em">${code}}</b>
+						 [${count}注,<em class="code-list-money">${count*2}</em>元]
+					 </div>
+				 </li>
+			`;
+			$(self.cart_el).append(tpl);
+			self.getTotal();
+		}
+		//计算盈利亏损
+		getCount(){
+			let self = this;
+			let active=$('.btn-boll-active').length;
+			let count = self.computeCount(active,self.cur_play);
+			let range = self.computeBonus(active,self.cur_play);
+			let money = count*2;
+			let win1 = range[0] - money;
+			let win2 = range[1] - money;
+			let tpl;
+			let c1 = (win1<0&&win2<0)?Math.abs(win1):win1;
+			let c2 = (win1<0&&win2<0)?Math.abs(win2):win2;
+			if(count === 0){
+				tpl = `您选了 <b class="red">0</b> 注，共 <b class="red">0</b> 元`
+			}else if(range[0] === range[1]){
+				tpl= `您选了 <b>${count}</b> 注，共 <b>${count*2}</b> 元  <em>若中奖，奖金：
+					<strong class="red">${range[0]}</strong> 元，
+					您将${win1>=0?'盈利':'亏损'}
+					<strong class=${win1>=0?'red':'green'}>${Math.abs(win1)}</strong> 元</em>`;
+			}else{
+				tpl=`您选了 <b>${count}</b> 注，共 <b>${count*2}</b> 元  <em>若中奖，奖金：
+					<strong class="red">${range[0]}</strong> 至 <strong class="red">${range[1]}</strong> 元，
+					您将${(win1<0&&win2<0)?'亏损':'盈利'}
+					<strong class="${win1>=0?'red':'green'}">${c1} </strong>
+					至 <strong class="${win2>=0?'red':'green'}"> ${c2} </strong>
+					元</em>`
+			}
+			$('.sel_info').html(tpl);
+		}
+		//计算总金额
+		getTotal(){
+			let count = 0;
+			$('.codelist li').each(function(index,item){
+				count += $(item).attr('count')*1
+			})
+			$('#count').text(count);
+			$('#money').text(count*2);
+		}
+		//生成随机数
+		getRandom(num){
+			let arr = [],index;
+			let number = Array.from(this.number);
+			while(num--){
+				index = Number.parseInt(Math.random()*number.length);
+				arr.push(number[index]);
+				number.aplice(index,1);
+			}
+			return arr.join(' ');
+		}
+		//添加随机号码
+		getRandomcode(e){
+			e.preventDefault();
+			let self =this;
+			let target = e.currentTarget;
+			let num =$(target).attr('count');
+			let play = this.cur_play.match(/\d+/g)[0];
+			if(num === 0){
+				$('self.cart_el').html('')
+			}else{
+				for(let i =0 ; i<num; i++){
+					self.addCodeItem(self.getRandom(play),self.cur_play,self.play_list.get(self.cur_play).name,1);
+				}
+			}
+			
+		}
 	}
+	export default Base
